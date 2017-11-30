126bafbe4490b6f944201b010151b006ae46c518
dudor <dudor@qq.com>
2017-11-30 18:19:14 +0800

    add elementui

---
+++ src/page/layout/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">side</el-aside>
+    <el-container>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.el-container {
+    margin-bottom: 40px;
+  }
+</style>
