183d8bd89651bece82887b1758d01049691039f5
dudor <dudor@qq.com>
2017-12-01 23:24:05 +0800

    disable eslint

--- src/page/layout/Layout.vue
+++ src/page/layout/Layout.vue
@@ -1,27 +1,67 @@
 <template>
   <el-container class="wrapper">
-  <el-header :style="{'background-color':'#409eff'}">Header</el-header>
+  <el-header style="text-align:right;background-color:#409eff;">
+    <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+  </el-header>
   <el-container>
-    <el-aside width="200px" class="menu">
-       <el-menu default-active="1">
-          <el-menu-item index="1">MENU1</el-menu-item>
-          <el-menu-item index="2">MENU2</el-menu-item>
-          <el-menu-item index="3">MENU3</el-menu-item>
-        </el-menu>
+    <el-aside>
+      <el-menu :collapse="isCollapse" default-active="2">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+    </el-menu>
     </el-aside>
-    <el-main class="content">Main</el-main>
+    <el-main>
+      <el-button @click="toggleMenu">collapse</el-button>
+    </el-main>
   </el-container>
 </el-container>
 </template>
 
 <script>
 export default {
-  name: 'layout'
-}
+  name: "layout",
+  data() {
+    return {
+      isCollapse: false
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isCollapse = !this.isCollapse;
+    }
+  }
+};
 </script>
 
-<style>
-.el-container {
-    margin-bottom: 40px;
-}
-</style>
